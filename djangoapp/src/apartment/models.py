bfafaf6e62086c87b34ea7033ec71d65a8268f51
xueytian <yitianx2@illinois.edu>
2020-07-24 23:27:15 -0500

    connect post and apartment

--- djangoapp/src/apartment/models.py
+++ djangoapp/src/apartment/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class apartment(models.Model):
     ApartmentID     =   models.AutoField(primary_key=True)
-    Name            =   models.CharField(max_length=60)
+    Name            =   models.CharField(max_length=60, unique=True)
     Description     =   models.TextField(blank=True)
     Location        =   models.TextField(blank=True)
     Email           =   models.CharField(max_length=30)
--- djangoapp/src/post/views.py
+++ djangoapp/src/post/views.py
@@ -22,9 +22,15 @@ def detail(request, Post_id):
     return render(request, 'post/detail.html', {'info': result[0]})
 
 def Insertrecord(request):
+    # return apartment list
+    with connection.cursor() as c:
+        c.execute("select Name, ApartmentID from apartment_apartment")
+        result = namedtuplefetchall(c)
+    
+    # form
     if request.method=='POST':
         if request.POST.get('Post_title') and request.POST.get('id_id') \
-            and request.POST.get('Apartment') and request.POST.get('Move_in_date')\
+            and request.POST.get('ApartmentID') and request.POST.get('Move_in_date')\
             and request.POST.get('Move_out_date') and request.POST.get('Price')\
             and request.POST.get('Bedroom') and request.POST.get('Bathroom') and request.POST.get('Duration'):
             
@@ -32,7 +38,7 @@ def Insertrecord(request):
             saverecord=post()
             saverecord.Post_title = request.POST.get('Post_title')
             saverecord.id_id = request.POST.get('id_id')
-            saverecord.Apartment = request.POST.get('Apartment')
+            saverecord.ApartmentID_id = request.POST.get('ApartmentID')
             saverecord.Pub_date = timezone.now()
             saverecord.Move_out_date = request.POST.get('Move_out_date')
             saverecord.Move_in_date = request.POST.get('Move_in_date')
@@ -40,25 +46,31 @@ def Insertrecord(request):
             saverecord.Bedroom = request.POST.get('Bedroom')
             saverecord.Bathroom = request.POST.get('Bathroom')
             saverecord.Duration = request.POST.get('Duration')
-
+            
+            # find apartment name in the apartment table
+            with connection.cursor() as c1:
+                c1.execute("select Name from apartment_apartment where ApartmentID = %s",[saverecord.ApartmentID_id])
+                apartment = namedtuplefetchall(c1)
+            saverecord.Apartment = apartment[0].Name
+            
             # if there is Description
             if request.POST.get('Description'):
                 saverecord.Description = request.POST.get('Description')
                 with connection.cursor() as c:
-                    c.execute(" insert into post_post(Post_title, id_id, Apartment, Pub_date, Move_out_date, Move_in_date, Price, Bedroom, Bathroom, Description, Duration)\
-                                value(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",[saverecord.Post_title, saverecord.id_id, saverecord.Apartment, saverecord.Pub_date, saverecord.Move_out_date, saverecord.Move_in_date, saverecord.Price, saverecord.Bedroom, saverecord.Bathroom, saverecord.Description, saverecord.Duration])
+                    c.execute(" insert into post_post(Post_title, id_id, ApartmentID_id, Pub_date, Move_out_date, Move_in_date, Price, Bedroom, Bathroom, Description, Duration, Apartment)\
+                                value(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",[saverecord.Post_title, saverecord.id_id, saverecord.ApartmentID_id, saverecord.Pub_date, saverecord.Move_out_date, saverecord.Move_in_date, saverecord.Price, saverecord.Bedroom, saverecord.Bathroom, saverecord.Description, saverecord.Duration, saverecord.Apartment])
             
             # no Description
             else:
                 with connection.cursor() as c:
-                    c.execute(" insert into post_post(Post_title, id_id, Apartment, Pub_date, Move_out_date, Move_in_date, Price, Bedroom, Bathroom, Duration)\
-                                value(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",[saverecord.Post_title, saverecord.id_id, saverecord.Apartment, saverecord.Pub_date, saverecord.Move_out_date, saverecord.Move_in_date, saverecord.Price, saverecord.Bedroom, saverecord.Bathroom, saverecord.Duration])
+                    c.execute(" insert into post_post(Post_title, id_id, ApartmentID_id, Pub_date, Move_out_date, Move_in_date, Price, Bedroom, Bathroom, Duration, Apartment)\
+                                value(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",[saverecord.Post_title, saverecord.id_id, saverecord.ApartmentID_id, saverecord.Pub_date, saverecord.Move_out_date, saverecord.Move_in_date, saverecord.Price, saverecord.Bedroom, saverecord.Bathroom, saverecord.Duration, saverecord.Apartment])
             messages.success(request, 'Post successfully')
-            return render(request, 'post/insertpost.html')
+            return render(request, 'post/insertpost.html', {'apartments': result})
         else:
-            return render(request, 'post/insertpost.html')
+            return render(request, 'post/insertpost.html', {'apartments': result})
     else:
-        return render(request, 'post/insertpost.html')
+        return render(request, 'post/insertpost.html', {'apartments': result})
 
 # user collections.namedtuple() from the Python standard library
 def namedtuplefetchall(cursor):
